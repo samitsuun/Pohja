453093f197dd99a5fdeebff7e2c6e9b73f11ac18
it15 <it15@labra.rmk.hamk.fi>
2018-01-26 10:12:46 +0200

    added calculatorthingie

---
+++ NUnitPohja/Testsit/MerkkijonolaskinTest.cs
@@ -0,0 +1,22 @@
+﻿using Harj;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testsit
+{
+    [TestFixture]
+    public class MerkkijonolaskinTest
+    {
+        [Test]  //tyhjä palauttaa nollan.
+        public void TyhjaMerkkijonoPalauttaNollan()
+        {
+            Merkkijonolaskin laskin = new Merkkijonolaskin();
+
+            Assert.That(laskin.Summa(""), Is.EqualTo(0));
+        }
+    }
+}
